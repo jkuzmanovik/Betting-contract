cdd9eaed9ae5bbf826fdef753b94e4e45af59802
jkuzmanovik <jordan.kuzmanovik@gmail.com>
2021-04-13 23:48:13 +0200

    betting contract

--- contracts/Betting.sol
+++ contracts/Betting.sol
@@ -3,69 +3,106 @@ pragma solidity >=0.4.22 <0.9.0;
 
 
 contract Betting {
-    // /* Constructor function, where owner and outcomes are set */
-    //  constructor(uint[] memory _outcomes) public {
-    // }
-
-    // /* Fallback function */
-    // // function() public payable {
-    // //     revert();
-    // // }
-
-    // /* Standard state variables */
-    // address public owner;
-    // address public gamblerA;
-    // address public gamblerB;
-    // address public oracle;
-
-    // /* Structs are custom data structures with self-defined parameters */
-    // struct Bet {
-    //     uint outcome;
-    //     uint amount;
-    //     bool initialized;
-    // }
-
-    // /* Keep track of every gambler's bet */
-    // mapping (address => Bet) bets;
-    // /* Keep track of every player's winnings (if any) */
-    // mapping (address => uint) winnings;
-    // /* Keep track of all outcomes (maps index to numerical outcome) */
-    // mapping (uint => uint) public outcomes;
-
-    // /* Add any events you think are necessary */
-    // event BetMade(address gambler);
-    // event BetClosed();
-
-    // /* Uh Oh, what are these? */
-    // modifier ownerOnly() {_;}
-    // modifier oracleOnly() {_;}
-    // modifier outcomeExists(uint outcome) {_;}
-
-    // /* Owner chooses their trusted Oracle */
-    // function chooseOracle(address _oracle) public ownerOnly() returns (address) {
-    // }
-
-    // /* Gamblers place their bets, preferably after calling checkOutcomes */
-    // function makeBet(uint _outcome) public payable returns (bool) {
-    // }
-
-    // /* The oracle chooses which outcome wins */
-    // function makeDecision(uint _outcome) public oracleOnly() outcomeExists(_outcome) {
-    // }
-
-    // /* Allow anyone to withdraw their winnings safely (if they have enough) */
-    // function withdraw(uint withdrawAmount) public returns (uint) {
-    // }
     
-    // /* Allow anyone to check the outcomes they can bet on */
-    // function checkOutcomes(uint outcome) public view returns (uint) {
-    // }
-    
-    // /* Allow anyone to check if they won any bets */
-    // function checkWinnings() public view returns(uint) {
-    // }
 
-    // /* Call delete() to reset certain state variables. Which ones? That's upto you to decide */
-    // function contractReset() public ownerOnly() {
-    // }
+    constructor(uint[] memory _outcomes) public{
+        for(uint i=0;i<_outcomes.length;i++){
+            outcomes[i] = _outcomes[i];
+        }
+        owner = msg.sender;
+    }
+
+
+    address  public owner;
+    address payable public gamblerA;
+    address payable public gamblerB;
+    address payable public oracle;
+
+
+
+    struct Bet{
+        uint outcome;
+        uint amount;
+        bool initialized; 
+    }
+
+    mapping(address => Bet) bets;
+
+    mapping(address => uint) winnings;
+
+    mapping(uint => uint) public outcomes;
+
+    event BetMade(address gambler);
+    event BetClosed();
+
+
+
+    modifier onlyOwner{
+        require(msg.sender == owner,
+        "Only owner can call this function"
+        );
+        _;
+    }
+    modifier onlyOracle{
+        require(msg.sender == oracle,
+        "Only oracle can call this function"
+        );
+        _;
+    }
+
+
+    function chooseOracle(address payable _oracle) public onlyOwner() returns (address){
+        oracle = _oracle;
+        return oracle;
+    }
+
+    function makeBet(uint _outcome) public payable returns (bool){
+        if(msg.value <= 0)
+            return false;
+        Bet memory bet;
+        bet = Bet(_outcome,msg.value,true);
+        bets[msg.sender] = bet;
+        return true;
+    }
+
+
+    function makeDecision(uint _outcome) public onlyOracle(){
+        uint pot = bets[gamblerA].amount + bets[gamblerB].amount;
+
+        if (bets[gamblerA].outcome == _outcome && bets[gamblerB].outcome == _outcome) {
+            gamblerA.transfer(bets[gamblerA].amount);
+            gamblerB.transfer(bets[gamblerB].amount);
+        } else if (bets[gamblerA].outcome == _outcome) {
+            winnings[gamblerA] += pot;
+        } else if (bets[gamblerB].outcome == _outcome) {
+            winnings[gamblerB] += pot;
+        } else {
+            winnings[oracle] += pot;
+        }
+
+    }
+
+    function withdraw(uint withdrawAmount)public payable returns (uint) {
+        address payable sender = payable(msg.sender);
+        if(winnings[msg.sender] > withdrawAmount){
+            winnings[msg.sender]-=withdrawAmount;
+            sender.transfer(withdrawAmount);
+            return withdrawAmount;
+        }
+    }
+
+    function checkOutcomes(uint outcome) public view returns(uint){
+       return outcomes[outcome]; 
+    }
+
+    function checkWinnings() public view returns(uint){
+        return winnings[msg.sender];
+    }
+      function contractReset() public {
+        delete gamblerA;
+        delete gamblerB;
+        delete oracle;
+    }
+
+
 }
